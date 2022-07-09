673e3fc58f5e2621d0345c2f60ac134637dc0fc3
cpyoufy <cpyoufy@gmail.com>
2022-07-09 10:50:32 +0800

    Initial Commit

---
+++ Doras-doom-v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Doras-doom-v2
+//
+//  Created by rgs on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Doras-doom-v2/Doras_doom_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Doras_doom_v2App.swift
+//  Doras-doom-v2
+//
+//  Created by rgs on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Doras_doom_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
