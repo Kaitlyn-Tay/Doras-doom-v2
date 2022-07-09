4e399417812d311a72f9d86e080ac4d2733e8a81
Folzo1 <chrisd1231@gmail.com>
2022-07-09 11:11:45 +0800

    milestone check 1

---
+++ Doras-doom-v2/Friends.swift
@@ -0,0 +1,17 @@
+//
+//  Friends.swift
+//  Doras-doom-v2
+//
+//  Created by T Krobot on 9/7/22.
+//
+
+import Foundation
+
+struct Friends {
+    
+    var name: String
+    var favDoraCharacter: String
+    var gender: String
+    var likeBigRedChicken: Bool = false
+    
+}
